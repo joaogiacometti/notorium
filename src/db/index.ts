6beda4d510d2f9ddc603d21dd60c4834f6345ecb
João Pedro Giacometti Guerreiro <j.guerreiro@unesp.br>
2026-03-01 16:45:20 -0300

    feat(db): use neon http driver for drizzle

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,5 @@
-import { drizzle } from "drizzle-orm/node-postgres";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 import { appEnv } from "@/env";
 
-export const db = drizzle(appEnv.DATABASE_URL);
+export const db = drizzle({ client: neon(appEnv.DATABASE_URL) });
