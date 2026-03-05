9c57c3730bdef38d40052ffb37e3f229a4e38dd5
João Pedro Giacometti Guerreir <j.guerreiro@unesp.br>
2026-03-05 10:41:13 -0300

    refactor(db): replace neon connection with generic postgres

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,9 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 import { appEnv } from "@/env";
 
-export const db = drizzle({ client: neon(appEnv.DATABASE_URL) });
+const pool = new Pool({
+  connectionString: appEnv.DATABASE_URL,
+});
+
+export const db = drizzle({ client: pool });
