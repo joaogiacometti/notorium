be65bed8239aa7d6b15224e6c0791a5f2e98a2bb
João Pedro Giacometti Guerreiro <j.guerreiro@unesp.br>
2026-05-09 16:04:34 -0300

    fix(editor): improve syntax highlight themes

--- src/lib/editor/tiptap-lowlight.ts
+++ src/lib/editor/tiptap-lowlight.ts
@@ -1,8 +1,3 @@
 import { common, createLowlight } from "lowlight";
 
 export const tiptapLowlight = createLowlight(common);
-
-tiptapLowlight.registerAlias({
-  csharp: ["c#", "C#", "cs", "CSharp"],
-  java: ["Java"],
-});
